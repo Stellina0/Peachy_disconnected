f78226804b6a99b8eea0f50ff4ca5964f353a87e
xDestinyx <92421013+xDestinyx@users.noreply.github.com>
2023-03-03 18:45:23 +0700

    Update ScriptLoadder.lua

--- ScriptLoadder.lua
+++ ScriptLoadder.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/Peachy/main/BF.lua'))();
 elseif game.PlaceId == 10941751747 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/Peachy/main/AnimeFightingTycoon.lua'))();
+elseif game.PlaceId == 11874473440 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/Peachy/main/CarFactory.lua'))();
 end;
